e8b0f02e4fb795d3b24cac72dae62895f17701be
arekadeoye <arek.adeoye@wilmingtonplc.com>
2018-04-11 13:45:50 +0100

    Added sample dataset

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Robot } from './robot.model';
 import { FormControl, Validators } from '@angular/forms';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +15,36 @@ export class AppComponent implements OnInit {
   yPos: FormControl;
   face: FormControl;
   reports: any[] = [];
+
+  exampleData = [{
+    step: [
+      { action: 'move'},
+      { action: 'report'}
+    ],
+    text: 'Move, Report'},
+    {   
+    step: [
+      { action: { 'place' : {x:0, y:0, f:'North'} }},
+      { action: 'report'}
+    ],
+    text: 'Place [0, 0, North], Report'},
+    {   
+    step: [
+      { action: { 'place' : {x:0, y:0, f:'North'} }},
+      { action: 'move'},
+      { action: 'move'},
+      { action: 'report'}
+    ],
+    text: 'Place [0, 0, North], Move, Move, Report'},
+    {   
+    step: [
+      { action: { 'place' : {x:1, y:2, f:'East'} }},
+      { action: { 'turn' : 'left' }},
+      { action: 'move'},
+      { action: 'report'}
+    ],
+    text: 'Place [1, 2, East], Turn left, Move, Report'},    
+  ]
 
   ngOnInit() {
     this.MyRobot = new Robot([]);
@@ -40,6 +71,29 @@ export class AppComponent implements OnInit {
   turn(direction) {
     this.MyRobot.Turn(direction);
     this.reports.push('Turn ' + direction);
+  }
+
+  sample() {
+    this.exampleData.map(data => {
+      this.reports.push('<strong>Sample set: ' + data.text + '</strong>');
+      data.step.map(step => {
+        switch (step.action) {
+          case 'move':
+            this.move();
+            return;
+          case 'report':
+            this.report();
+            return;
+          default:
+            if (step.action['place']) {
+              this.MyRobot.Place(step.action['place'].x, step.action['place'].y, step.action['place'].f);
+            }
+            else if (step.action['turn']){
+              this.turn(step.action['turn']);
+            }
+        }
+      })
+    });
   }
 
 }
